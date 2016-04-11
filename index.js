85de39788acc5806a23437e50e59f1989ed925fc
Chris Kwan <chriswkwan@gmail.com>
2016-04-11 06:59:12 -0400

    Create links for recorded videos

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // NOTE: this code will only work if running this code from a server
 // (e.g. http-server) instead of a local file
+// Ref: https://www.webrtc-experiment.com/msr/MultiStreamRecorder.html
 (function() {
 	var multiStreamRecorder;
 	var videoElement = document.getElementById("msrVideo");
@@ -7,6 +8,7 @@
 	var stopButton = document.getElementById("stopBtn");
 	var playButton = document.getElementById("playBtn");
 	var pauseButton = document.getElementById("pauseBtn");
+	var recordings = document.getElementById("recordings");
 
 	startButton.onclick = startRecording;
 	stopButton.onclick = stopRecording;
@@ -26,11 +28,23 @@
 		multiStreamRecorder.audioChannels = 1;
 		
 		multiStreamRecorder.ondataavailable = function (blobs) {
-			console.log("on data available");
+			console.log("ON DATA AVAILABLE");
+			makeLink(blobs.video);
 		};
 
 		// Show the stream shows up in the video element
 		videoElement.src = URL.createObjectURL(stream);
+	}
+
+	function makeLink(blob) {
+		// Ref: https://www.webrtc-experiment.com/msr/MultiStreamRecorder.html
+		var a = document.createElement('a');
+		a.target = "_blank";
+		a.innerHTML = "Open Video Recording"; //cwkTODO add number, size, time
+		
+		a.href = URL.createObjectURL(blob);
+
+		recordings.appendChild(a);
 	}
 
 	function onMediaError(e) {
